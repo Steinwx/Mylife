65eb24343b1a53977184b187c831f9863eac72be
Steinway <adrian.montas232@gmail.com>
2023-07-31 17:06:57 +0800

    replaced with code

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-Write a method in python to write multiple line of text contents into a text file mylife.txt. See sample output:
-Enter line: Beautiful is better than ugly.
-Are there more lines y/n? y
-Enter line: Explicit is better than implicit.
-Are there more lines y/n? y
-Enter line: Simple is better than complex.
-Are there more lines y/n? n 
+def write_multiple_lines_to_file(file_name):
+    with open(file_name, 'w') as file:
+        while True:
+            line = input("Enter line: ")
+            file.write(line + '\n')
+            more_lines = input("Are there more lines? (y/n): ")
+            if more_lines.lower() != 'y':
+                break
+
+if __name__ == "__main__":
+    write_multiple_lines_to_file("mylife.txt")
